2290788e70ddb7bed4a3667deae91812c6a3e378
ggs9801 <ggs9801@gmail.com>
2021-12-01 21:14:02 +0900

    Algorithm Lv.2 Method Update

--- GA/Program_Lv2.cs
+++ GA/Program_Lv2.cs
@@ -16,40 +16,75 @@ namespace 알고리즘
             return randNormal;
         }
 
-        static void Main(string[] args)
+        public static double[,] FirstVaccinatingCreate(int endDay)
         {
-            int population = 50000000;
-            double youngRate = 0.167; // 영유아 비율 (10대 이하)
+            // GA의 기본적인 기본 //
+
+            double[,] dayVaccinatingList = new double[3, endDay]; // 날짜별 백신 접종 비율 리스트
+
+            for (int i = 0; i < 3650; i++) // 날짜별 백신 접종 비율 배열 생성
+            {
+                Random rand = new Random();
+                double youngVacRate = rand.Next(1000) / 1000;
+                double adultVacRate = rand.Next(1000 - (int)(youngVacRate * 1000)) / 1000;
+                double oldVacRate = 1 - (youngVacRate + adultVacRate);
+
+                dayVaccinatingList[0, i] = youngVacRate;
+                dayVaccinatingList[1, i] = adultVacRate;
+                dayVaccinatingList[2, i] = oldVacRate;
+            }
+
+            return dayVaccinatingList;
+        }
+
+        public static int FitnessCheck()
+        {
+            int population = 50000000; // 인구수 설정
+            double youngRate = 0.167; // 미성년자 비율 (10대 이하)
             double adultRate = 0.587; // 청장년 비율 (20대 ~ 50대)
             double oldRate = 1 - (youngRate + adultRate); // 노인 비율 (50대 이상)
 
-            int youngPop = (int)(population * youngRate);
-            int adultPop = (int)(population * adultRate);
-            int oldPop = population - (youngPop + adultPop);
+            int[] popList = new int[3]; // 각 연령대별 인구수 배열
 
-            double transmissionRate = 0.0089;
-            double youngFatalityRate = 0.0001;      // 영유아 치명률
-            double adultFatalityRate = 0.00104;     // 청장년 치명률
-            double oldFatalityRate = 0.034796;      // 노인 치명률
+            popList[0] = (int)(population * youngRate); // 미성년자 인구수 초기설정
+            popList[1] = (int)(population * adultRate); // 청장년 인구수 초기설정
+            popList[2] = (int)(population * oldRate); // 노인 인구수 초기설정
 
-            double vaccinatedRate = 0;
-            double infectionsRate = 0;
+            double transmissionRate = 0.0089; // 감염률
+            double vaccinatedTransRate = 0.2; // 백신 맞았을 경우에 감염률
 
-            int youngdayInfects = 0;                // 영유아 하루 감염자 수
-            int adultdayInfects = 0;                // 청장년 하루 감염자 수
-            int olddayInfects = 0;                  // 노인 하루 감염자 수
+            double[] fatalityRateList = new double[3]; // 각 인구별 치명률 배열
 
-            int numberOfVaccinated = 0;             // 백신 접종자 수
-            int numberOfDeaths = 0;                 // 사망자 수
-            int numberOfInfections = 14;            // 초기 감염자 숫자
+            fatalityRateList[0] = 0.0001; // 미성년자 치명률
+            fatalityRateList[1] = 0.00104; // 청장년 치명률
+            fatalityRateList[2] = 0.034796; // 노인 치명률
 
-            int day = 0;
-            int endDay = 3650;                      // 끝나는 일수
-            int[] dayList = new int[endDay];
-            int[] infectedList = new int[endDay];
-            int[] vaccinatedList = new int[endDay];
-            int[] deadList = new int[endDay];
-            int[] infectsPerDayList = new int[endDay];
+            double vaccinatedFatalityRate = 1 - 0.98; // 백신 맞았을 경우에 치명률
+
+            double vaccinatedRate = 0; // 전체 인구 중 접종자 비율
+            double infectionsRate = 0; // 전체 인구 중 감염자 비율
+
+            int[] dayinfectsList = new int[3];
+
+            dayinfectsList[0] = 0; // 미성년자 하루 감염자 수          
+            dayinfectsList[1] = 0; // 청장년 하루 감염자 수
+            dayinfectsList[2] = 0; // 노인 하루 감염자 수
+
+            int numberOfVaccinated = 0; // 백신 접종자 수
+            int numberOfDeaths = 0; // 사망자 수
+            int numberOfInfections = 14; // 초기 감염자 숫자
+
+            int[] numberofInfectionsList = new int[3]; // 각 인구별 감염자 수
+
+            //// 기록 변수들 ////
+
+            int day = 0; // 날짜 Counting
+            int endDay = 3650; // 끝나는 일수
+            int[] dayList = new int[endDay]; // 날짜 (그래프 X축)
+            int[,] infectedList = new int[3, endDay]; // 연령대별 누적 감염자 수 기록
+            int[,] vaccinatedList = new int[3, endDay]; // 연령대별 백신 접종자 수 기록
+            int[,] deadList = new int[3, endDay]; // 연령대별 사망자 수 기록
+            int[,] infectsPerDayList = new int[3, endDay]; // 
             int threatingDay = 14;
 
             int numberOfContacts;
@@ -64,19 +99,9 @@ namespace 알고리즘
             mu_s = 400000;
             sigma_s = 50000;
 
-            double[,] dayVaccinatedList = new double[3, endDay]; // 날짜별 백신 접종 비율 리스트
 
-            for (int i = 0; i < 3650; i++) // 날짜별 백신 접종 비율 배열 생성
-            {
-                Random rand = new Random();
-                double youngVacRate = rand.Next(1000)/1000;
-                double adultVacRate = rand.Next(1000 - (int)(youngVacRate * 1000)) / 1000;
-                double oldVacRate = 1 - (youngVacRate + adultRate);
 
-                dayVaccinatedList[0, i] = youngVacRate;
-                dayVaccinatedList[1, i] = adultVacRate;
-                dayVaccinatedList[2, i] = oldVacRate;
-            }
+            // 실제 Simulation
 
             while (vaccinatedRate < 1 && numberOfInfections < population && day < endDay)
             {
@@ -88,6 +113,8 @@ namespace 알고리즘
                 youngRate = (double)(youngPop / population);
                 adultRate = (double)(adultPop / population);
                 oldRate = (double)(oldPop / population);
+
+
 
                 dayList[day] = day;
                 infectedList[day] = numberOfInfections;
@@ -104,7 +131,7 @@ namespace 알고리즘
                 preventionRate = norm(0.9, 0.05);
                 shotPerDay = (int)(norm(mu_s, sigma_s));
 
-                youngdayInfects = (int)(numberOfInfections * numberOfContacts * transmissionRate * (1 - vaccinatedRate) + rand.Next(20, 31)) * youngRate;
+                youngdayInfects = (int)(numberOfInfections * numberOfContacts * transmissionRate * (1 - vaccinatedRate) + rand.Next(20, 31) * youngRate);
                 infectsPerDayList[day] = dayInfects;
 
                 numberOfInfections += dayInfects;
@@ -116,7 +143,7 @@ namespace 알고리즘
                     if (infectedList[day - 1] > 500)
                         numberOfInfections -= infectsPerDayList[day - threatingDay];
                 }
-                
+
                 infectionsRate = numberOfInfections / (double)population;
                 numberOfDeaths = (int)(numberOfInfections * fatalityRate);
                 population -= numberOfDeaths;
@@ -136,19 +163,21 @@ namespace 알고리즘
 
                 day++;
 
-                
+
 
                 /// Console.WriteLine("{0}", numberOfInfections);
             }
 
-
-
-
-
-
             Console.WriteLine("최종 감염자 : {0}", numberOfInfections);
             Console.WriteLine("백신 접종 : {0}", numberOfVaccinated);
             Console.WriteLine("생존자: {0}", population);
+
+            return population;
+        }
+
+        static void Main(string[] args)
+        {
+            FitnessCheck();
         }
     }
 
