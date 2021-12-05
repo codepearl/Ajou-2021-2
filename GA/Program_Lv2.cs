e9c66d989ef195c8b3394f6210283f3a96004829
ggs9801 <ggs9801@gmail.com>
2021-12-05 18:28:45 +0900

    Completed FitnessCheck Method

--- GA/Program_Lv2.cs
+++ GA/Program_Lv2.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace 알고리즘
 {
@@ -210,12 +213,211 @@ namespace 알고리즘
 
             //// 실제 Simulation 시작 ////
 
-            while (vaccinatedRate < 1 && numberOfInfections < population && day < endDay)
+            while (vaccinatedRate < 1 & numberOfInfections < population & day < endDay)
             {
                 // 하루 일과 : 집계-> 감염 -> 사망 -> 백신 접종 -> 마감
-                Console.WriteLine("날짜 : " + day);
+                // Console.WriteLine("");
+                // Console.WriteLine("날짜 : " + day);
 
                 Random rand = new Random();
+
+
+
+                //// 감염 단계 ////
+                numberOfContacts = (int)norm(mu_c, sigma_c); // 접촉자 수 선정
+                numberOfContacts = (int)(numberOfContacts * (1 - Math.Pow(infectionsRate, 0.05)) * Math.Pow(Math.Max(1, day - 1500), 0.01) + 3);
+
+                // print(numberOfContacts, infectionsRate, numberOfInfections)
+                // 코로나가 심해질수록 밖에 안나감, 3명은 무조건 만남
+                // 1500일 이후 위드코로나 진행 (만나는 사람 증가로 인한 2차 팬데믹 유도)
+
+                // Console.Write("감염자 : ");
+                for (int i = 0; i < 3; i++)
+                {
+                    dayinfectsList[i] = (int)((numberOfInfections * (double)numberOfContacts * transmissionRate * (1 - vaccinatedRate) * (1 - infectionsRate)) * popRateList[i]);
+                    dayinfectsList[i] += (int)((double)rand.Next(20, 31) * popRateList[i] * (1 - vaccinatedRate));
+                    dayVaccinatedInfectsList[i] = (int)((numberOfInfections * (double)numberOfContacts * transmissionRate * vaccinatedTransRate * vaccinatedRate * (1 - infectionsRate)) * popRateList[i]);
+                    dayVaccinatedInfectsList[i] += (int)(rand.Next(20, 31) * popRateList[i] * vaccinatedRate);
+
+                    // 감염자로 인한 전파 + 외부 유입 (20~30)
+                    numberofInfectionsList[i] += dayinfectsList[i]; // 연령별 총 감염자수 추가
+                    numberofVaccinatedInfectionsList[i] += dayVaccinatedInfectsList[i];
+
+                    // Console.Write(numberofInfectionsList[i] + " ");
+                    // Console.Write(numberofVaccinatedInfectionsList[i] + " ");
+                }
+                // Console.WriteLine("");
+
+
+
+                //// 사망 단계 ////
+                for (int i = 0; i < 3; i++)
+                {
+                    dayDeathsList[i] = (int)(numberofInfectionsList[i] * fatalityRateList[i]);
+                    numberofInfectionsList[i] -= dayDeathsList[i];
+                    popList[i] -= dayDeathsList[i];
+
+                    int dayCount = 1;
+                    for (int j = dayDeathsList[i]; j < 0; j = j - rand.Next(0, Math.Max(0, (int)j)))
+                    {
+                        if (dayCount == threatingDay - 1)
+                        {
+                            dayCount = 1;
+                        }
+                        infectsPerDayList[i, day - dayCount] -= j;
+
+                        if (infectsPerDayList[i, day - dayCount] < 0)
+                        {
+                            j += (0 - infectsPerDayList[i, day - dayCount]);
+                            infectsPerDayList[i, day - dayCount] = 0;
+                        }
+
+                        dayCount = dayCount + 1;
+                    }
+
+                    dayDeathsList[i] = (int)(numberofVaccinatedInfectionsList[i] * fatalityRateList[i] * vaccinatedFatalityRate);
+                    numberOfVaccinatedList[i] -= dayDeathsList[i];
+                    numberofVaccinatedInfectionsList[i] -= dayDeathsList[i];
+                    popList[i] -= dayDeathsList[i];
+
+                    dayCount = 1;
+                    for (int j = dayDeathsList[i]; j < 0; j = j - rand.Next(0, Math.Max(0, (int)j)))
+                    {
+                        if (dayCount == threatingDay - 1)
+                        {
+                            dayCount = 1;
+                        }
+                        vaccinatedinfectsPerDayList[i, day - dayCount] -= j;
+
+                        if (vaccinatedinfectsPerDayList[i, day - dayCount] < 0)
+                        {
+                            j += (0 - vaccinatedinfectsPerDayList[i, day - dayCount]);
+                            vaccinatedinfectsPerDayList[i, day - dayCount] = 0;
+                        }
+
+                        dayCount = dayCount + 1;
+                    }
+                }
+
+
+
+                //// 백신 접종 단계 ////
+                preventionRate = rand.Next(850, 950) / (double)1000;
+                shotPerDay = (int)(norm(mu_s, sigma_s));
+
+                int reallyshot;
+                // Console.Write("백신 접종량 : ");
+                for (int i = 0; i < 3; i++)
+                {
+                    shotPerDayList[i] = (int)(shotPerDay * VaccineScheduleList[i, day]);
+
+                    reallyshot = (int)(shotPerDayList[i] * preventionRate * (1 - vaccinatedRate) * Math.Max(infectionsRate, 0.01));
+                    if (reallyshot < 0)
+                        reallyshot = 0;
+
+                    if (numberOfVaccinatedList[i] + reallyshot < popList[i])
+                        numberOfVaccinatedList[i] += reallyshot;
+                    else
+                    {
+                        reallyshot = popList[i] - numberOfVaccinatedList[i];
+                        numberOfVaccinatedList[i] = popList[i];
+                    }
+                    // Console.Write(numberOfVaccinatedList[i] + " ");
+                    // Console.Write(reallyshot + " ");
+
+                    // 확진자가 많아질수록 백신 희망자가 늘어남
+                    // 백신 접종률이 높아질수록 안이한 생각에 맞지 않으려 함
+                    // 0.01%의 국민은 무조건 하루에 백신을 맞추려고 함.
+                    int changingnumber = 0;
+                    if ((popList[i] - numberOfVaccinatedList[i]) > 0)
+                        changingnumber = (int)(numberofInfectionsList[i] / (double)(popList[i] - numberOfVaccinatedList[i]) * reallyshot);
+
+                    numberofInfectionsList[i] -= changingnumber;
+                    numberofVaccinatedInfectionsList[i] += changingnumber;
+                    int dayCount = 1;
+
+                    for (int j = changingnumber; j < 0; j = j - rand.Next(0, Math.Max(0, (int)j)))
+                    {
+
+                        if (dayCount == threatingDay - 1)
+                        {
+                            dayCount = 1;
+                        }
+                        infectsPerDayList[i, day - dayCount] -= j;
+                        vaccinatedinfectsPerDayList[i, day - dayCount] += j;
+
+                        if (infectsPerDayList[i, day - dayCount] < 0)
+                        {
+                            j += (0 - infectsPerDayList[i, day - dayCount]);
+                            infectsPerDayList[i, day - dayCount] = 0;
+                        }
+
+                        dayCount = dayCount + 1;
+                    }
+
+                }
+                // Console.WriteLine("");
+
+
+                //// 치료 단계 ////
+                if (day > threatingDay)
+                {
+                    int totalInfected = 0;
+                    // Console.Write("치료 될 감염자 : ");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        totalInfected += numberofInfectionsList[i] + numberofVaccinatedInfectionsList[i];
+                        // Console.Write(infectsPerDayList[i, day - threatingDay] + " ");
+                        // Console.Write(vaccinatedinfectsPerDayList[i, day - threatingDay] + " ");
+                    }
+                    // Console.WriteLine("");
+                    // Console.Write("치료 후 총감염자 : ");
+                    if (totalInfected > 100)
+                        for (int i = 0; i < 3; i++) // 연령별 감염자 치료날짜 이후 비감염자 됨
+                        {
+                            numberofInfectionsList[i] -= infectsPerDayList[i, day - threatingDay];
+                            if (numberofInfectionsList[i] < 0)
+                                numberofInfectionsList[i] = 0;
+
+                            numberofVaccinatedInfectionsList[i] -= vaccinatedinfectsPerDayList[i, day - threatingDay];
+                            if (numberofVaccinatedInfectionsList[i] < 0)
+                                numberofVaccinatedInfectionsList[i] = 0;
+
+                            // Console.Write(numberofInfectionsList[i] + " ");
+                            // Console.Write(numberofVaccinatedInfectionsList[i] + " ");
+                        }
+
+                }
+                // Console.WriteLine(" ");
+
+
+                //// 집계 단계 ////
+
+                population = popList[0] + popList[1] + popList[2]; // 인구 집계
+                // Console.Write("인구수 : ");
+                for (int i = 0; i < 3; i++)
+                {
+                    // Console.Write("{0} ", popList[i]);
+                    popRateList[i] = popList[i] / (double)population;
+                }
+                // Console.WriteLine(" ");
+
+                dayRecord[day] = day; // 날짜 기록
+
+                for (int i = 0; i < 3; i++)
+                {
+                    infectsPerDayList[i, day] = dayinfectsList[i]; // 하루 미접종자 감염자 수 기록
+                    vaccinatedinfectsPerDayList[i, day] = dayVaccinatedInfectsList[i]; // 하루 접종자 감염자 수 기록
+                    infectedRecord[i, day] = numberofInfectionsList[i] + numberofVaccinatedInfectionsList[i]; // 총 감염자 수 기록
+                    deadRecord[i, day] = startPop - popList[i]; // 사망자 기록
+                }
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    numberOfVaccinated += numberOfVaccinatedList[i]; // 총 백신 접종량 집계
+                    vaccinatedRecord[i, day] = numberOfVaccinatedList[i]; // 백신 접종자 수 기록
+                }
 
                 numberOfInfections = 0;
                 numberOfVaccinated = 0;
@@ -229,179 +431,14 @@ namespace 알고리즘
                 vaccinatedRate = (numberOfVaccinated / (double)population);
 
 
-
-                //// 감염 단계 ////
-                numberOfContacts = (int)norm(mu_c, sigma_c); // 접촉자 수 선정
-                numberOfContacts = (int)(numberOfContacts * (1 - Math.Pow(infectionsRate, 0.1)) * Math.Pow(Math.Max(1, day - 1500), 0.05) + 3);
-                // print(numberOfContacts, infectionsRate, numberOfInfections)
-                // 코로나가 심해질수록 밖에 안나감, 3명은 무조건 만남
-                // 1500일 이후 위드코로나 진행 (만나는 사람 증가로 인한 2차 팬데믹 유도)
-
-
-                for (int i = 0; i < 3; i++)
-                {
-                    dayinfectsList[i] = (int)((numberOfInfections * numberOfContacts * transmissionRate * (1 - vaccinatedRate)) * popRateList[i]);
-                    dayinfectsList[i] += (int)(rand.Next(20, 31) * popRateList[i] * (1 - vaccinatedRate));
-                    dayVaccinatedInfectsList[i] = (int)((numberOfInfections * numberOfContacts * transmissionRate * vaccinatedTransRate * vaccinatedRate) * popRateList[i]);
-                    dayVaccinatedInfectsList[i] += (int)(rand.Next(20, 31) * popRateList[i] * vaccinatedRate);
-
-                    // 감염자로 인한 전파 + 외부 유입 (20~30)
-                    numberofInfectionsList[i] += dayinfectsList[i]; // 연령별 총 감염자수 추가
-                    numberofVaccinatedInfectionsList[i] += dayVaccinatedInfectsList[i];
-
-                }
-
-
-
-                //// 사망 단계 ////
-                for (int i = 0; i < 3; i++)
-                {
-                    dayDeathsList[i] = (int)(numberofInfectionsList[i] * fatalityRateList[i]);
-                    numberofInfectionsList[i] -= dayDeathsList[i];
-                    popList[i] -= dayDeathsList[i];
-
-
-                    dayDeathsList[i] = (int)(numberofVaccinatedInfectionsList[i] * fatalityRateList[i] * vaccinatedFatalityRate);
-                    numberOfVaccinatedList[i] -= dayDeathsList[i];
-                    numberofVaccinatedInfectionsList[i] -= dayDeathsList[i];
-                    popList[i] -= dayDeathsList[i];
-
-
-                    int dayCount = 1;
-                    for (int j = dayDeathsList[i]; j < 0; j = j - rand.Next(0, Math.Max(0, j)))
-                    {
-                        if (dayCount == threatingDay)
-                        {
-                            infectsPerDayList[i, day - dayCount] -= j;
-                            vaccinatedinfectsPerDayList[i, day - dayCount] += j;
-                            break;
-                        }
-                        infectsPerDayList[i, day - dayCount] -= j;
-
-
-                        if (infectsPerDayList[i, day - dayCount] < 0)
-                            j += 0 - infectsPerDayList[i, day - dayCount];
-
-                        if (vaccinatedinfectsPerDayList[i, day - dayCount] < 0)
-                            j += 0 - vaccinatedinfectsPerDayList[i, day - dayCount];
-
-                        dayCount = dayCount + 1;
-                    }
-
-
-
-
-                }
-
-
-
-                //// 백신 접종 단계 ////
-                preventionRate = norm(0.9, 0.05);
-                shotPerDay = (int)(norm(mu_s, sigma_s));
-
-                int reallyshot;
-
-                for (int i = 0; i < 3; i++)
-                {
-                    shotPerDayList[i] = (int)(shotPerDay * VaccineScheduleList[i, day]);
-
-                    reallyshot = (int)(shotPerDayList[i] * preventionRate * (1 - vaccinatedRate) * Math.Max(infectionsRate, 0.02));
-
-                    if (numberOfVaccinatedList[i] + reallyshot < popList[i])
-                        numberOfVaccinatedList[i] += reallyshot;
-                    else
-                    {
-                        reallyshot = popList[i] - numberOfVaccinatedList[i];
-                        numberOfVaccinatedList[i] = popList[i];
-                    }
-
-                    // 확진자가 많아질수록 백신 희망자가 늘어남
-                    // 백신 접종률이 높아질수록 안이한 생각에 맞지 않으려 함
-                    // 0.01%의 국민은 무조건 하루에 백신을 맞추려고 함.
-
-
-                    int changingnumber = (int)(numberofInfectionsList[i] / (double)(popList[i] - numberOfVaccinatedList[i]) * reallyshot);
-
-
-                    numberofInfectionsList[i] -= changingnumber;
-                    numberofVaccinatedInfectionsList[i] += changingnumber;
-                    int dayCount = 1;
-
-                    for (int j = changingnumber; j < 0; j = j - rand.Next(0, Math.Max(0, j)))
-                    {
-                        if (dayCount == threatingDay)
-                        {
-                            infectsPerDayList[i, day - dayCount] -= j;
-                            vaccinatedinfectsPerDayList[i, day - dayCount] += j;
-                            break;
-                        }
-                        infectsPerDayList[i, day - dayCount] -= j;
-
-
-                        if (infectsPerDayList[i, day - dayCount] < 0)
-                            j += 0 - infectsPerDayList[i, day - dayCount];
-
-                        if (vaccinatedinfectsPerDayList[i, day - dayCount] < 0)
-                            j += 0 - vaccinatedinfectsPerDayList[i, day - dayCount];
-
-                        dayCount = dayCount + 1;
-                    }
-                }
-
-                //// 치료 단계 ////
-                if (day > threatingDay)
-                {
-                    int totalInfected = 0;
-                    for (int i = 0; i < 3; i++)
-                        totalInfected += numberofInfectionsList[i] + numberofVaccinatedInfectionsList[i];
-
-                    if (totalInfected > 100)
-                        for (int i = 0; i < 3; i++) // 연령별 감염자 치료날짜 이후 비감염자 됨
-                        {
-                            numberofInfectionsList[i] -= infectsPerDayList[i, day - threatingDay];
-                            if (numberofInfectionsList[i] < 0)
-                                numberofInfectionsList[i] = 0;
-                            numberofVaccinatedInfectionsList[i] -= vaccinatedinfectsPerDayList[i, day - threatingDay];
-                            if (numberofVaccinatedInfectionsList[i] < 0)
-                                numberofVaccinatedInfectionsList[i] = 0;
-                        }
-
-                }
-
-
-
-                //// 집계 단계 ////
-
-                population = popList[0] + popList[1] + popList[2]; // 인구 집계
-
-                for (int i = 0; i < 3; i++)
-                    popRateList[i] = popList[i] / (double)population;
-
-                dayRecord[day] = day; // 날짜 기록
-
-                for (int i = 0; i < 3; i++)
-                {
-                    infectsPerDayList[i, day] = dayinfectsList[i]; // 하루 미접종자 감염자 수 기록
-                    vaccinatedinfectsPerDayList[i, day] = dayVaccinatedInfectsList[i]; // 하루 접종자 감염자 수 기록
-                    infectedRecord[i, day] = numberofInfectionsList[i]; // 총 감염자 수 기록
-                    deadRecord[i, day] = startPop - popList[i]; // 사망자 기록
-                }
-
-
-                for (int i = 0; i < 3; i++)
-                {
-                    numberOfVaccinated += numberOfVaccinatedList[i]; // 총 백신 접종량 집계
-                    vaccinatedRecord[i, day] = numberOfVaccinatedList[i]; // 백신 접종자 수 기록
-                }
-
                 day++; // 하루 일과 마침
-                Console.WriteLine("백신 접종 : {0}", numberOfVaccinated);
-                Console.WriteLine("최종 감염자 : {0}", numberOfInfections);
-                Console.WriteLine("생존자: {0}", population);
+                // Console.WriteLine("백신 접종 : {0}, {1}", numberOfVaccinated, vaccinatedRate);
+                // Console.WriteLine("최종 감염자 : {0}, {1}", numberOfInfections,infectionsRate);
+                // Console.WriteLine("생존자: {0}", population);
             }
-            Console.WriteLine("최종 날짜 : {0}", day);
-            Console.WriteLine("최종 감염자 : {0}", numberOfInfections);
-            Console.WriteLine("백신 접종 : {0}", numberOfVaccinated);
+            //Console.WriteLine("최종 날짜 : {0}", day);
+            //Console.WriteLine("백신 접종 : {0}, {1}", numberOfVaccinated, vaccinatedRate);
+            //Console.WriteLine("최종 감염자 : {0}, {1}", numberOfInfections, infectionsRate);
             Console.WriteLine("생존자: {0}", population);
 
             return population;
@@ -409,8 +446,27 @@ namespace 알고리즘
 
         static void Main(string[] args)
         {
-            FitnessCheck(0, null);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int numberOfSon = 32;
+
+            Parallel.For(0, numberOfSon, i =>
+            {
+                for (int j = 0; j < 10; j++)
+                    FitnessCheck(0, null);
+            });
+
+
+            sw.Stop();
+
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
+
+            // millisecond -> second
+            double elapsedSecond = (elapsedMilliseconds / 1000.0);
+            Console.WriteLine("구동시간 : " + elapsedSecond + "초");
         }
     }
 
+}
 }
